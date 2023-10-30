b9d53fd12e2327033686fd41aedd529863895d8e
Evany Silvestre <evany.120@gmail.com>
2023-10-30 12:05:47 -0300

    Atividade e criar objetos em python

---
+++ ob.py
@@ -0,0 +1,55 @@
+class LoginAluno:
+  def __init__ (self, id_usuario, senha):
+    self.id_usuario = id_usuario
+    self.senha = senha
+
+id_usuario = input("Aluno, Insira seu email ou nome do usuário: ")
+senha = input("Agora, digite sua senha: ")
+
+class Forum:
+  def __init__(self, titulo, descricao):
+    self.titulo = titulo
+    self.descricao = descricao
+
+titulo = input("Informe o título: ")
+descricao = input("Sobre o que você deseja falar: ")
+
+class CadastroAluno:
+  def __init__ (self, nomealuno, data_nascimentoaluno, sexoaluno, emailaluno, id_usuarioaluno):
+    self.nomealuno = nomealuno
+    self.data_nascimentoaluno = data_nascimentoaluno
+    self.sexoaluno = sexoaluno
+    self.id_usuarioaluno = id_usuarioaluno
+    self.senhaaluno = self.senhaaluno
+    self.emailaluno = email
+
+nome = input("Informe o seu nome: ")
+data_nascimento = int(input("Digite sua Data de Nascimento: "))
+sexo = input("Informe o sexo: ")
+id_usuarioaluno = input("Digite um nome para o usuário: ")
+senha = input("Crie uma senha: ")
+email = input("Informe o seu email: ")
+
+class LoginProf:
+    def __init__(self, id_prof, senhap):
+      self.id_prof = id_usuario
+      self.senhap = senha
+
+id_prof = ("Professor, Insira seu email ou nome do usuário: ")
+senhap = ("Agora, digite sua senha: ")
+
+class CadastroProf:
+  def __init__ (self, nomeprof, data_nascimentoprof, sexoprof, emailprof, id_usuarioprof, senhaprof):
+    self.nomeprof = nomeprof
+    self.data_nascimentoprof = data_nascimentoprof
+    self.sexoprof = sexoprof
+    self.id_usuarioprof = id_usuarioprof
+    self.senhaprof = senhaprof
+    self.emailprof = emailprof
+
+nomeprof = input("Informe o seu nome: ")
+data_nascimentoprof = int(input("Digite sua Data de Nascimento: "))
+sexoprof = input("Informe o sexo: ")
+id.usuarioprof = input("Digite um nome para o usuário: ")
+senhaprof = input("Crie uma senha: ")
+emailprof = input("Informe o seu email: ")
