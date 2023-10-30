bc3e32cc1ef67be42e53fb699c6146ee3fa209e6
Evany Silvestre <evany.120@gmail.com>
2023-10-30 13:01:00 -0300

    Corrigido erros no código

--- ob.py
+++ ob.py
@@ -3,16 +3,24 @@ class LoginAluno:
     self.id_usuario = id_usuario
     self.senha = senha
 
+print("====================TELA DE LOGIN DO ALUNO====================\n")
+
 id_usuario = input("Aluno, Insira seu email ou nome do usuário: ")
 senha = input("Agora, digite sua senha: ")
+
+print("==============================================================\n")
 
 class Forum:
   def __init__(self, titulo, descricao):
     self.titulo = titulo
     self.descricao = descricao
 
+print("======================TELA DO FÓRUM======================\n")
+
 titulo = input("Informe o título: ")
 descricao = input("Sobre o que você deseja falar: ")
+
+print("==========================================================\n")
 
 class CadastroAluno:
   def __init__ (self, nomealuno, data_nascimentoaluno, sexoaluno, emailaluno, id_usuarioaluno):
@@ -20,23 +28,31 @@ class CadastroAluno:
     self.data_nascimentoaluno = data_nascimentoaluno
     self.sexoaluno = sexoaluno
     self.id_usuarioaluno = id_usuarioaluno
-    self.senhaaluno = self.senhaaluno
-    self.emailaluno = email
+    self.senhaaluno = senhaaluno
+    self.emailaluno = emailaluno
 
-nome = input("Informe o seu nome: ")
+print("====================TELA DO CADASTRO DO ALUNO====================\n")
+
+nome = input("Aluno, Informe o seu nome: ")
 data_nascimento = int(input("Digite sua Data de Nascimento: "))
 sexo = input("Informe o sexo: ")
-id_usuarioaluno = input("Digite um nome para o usuário: ")
+id_usuarioaluno = input("Aluno, Digite um nome para o usuário: ")
 senha = input("Crie uma senha: ")
 email = input("Informe o seu email: ")
 
+print("=================================================================\n")
+
 class LoginProf:
     def __init__(self, id_prof, senhap):
-      self.id_prof = id_usuario
-      self.senhap = senha
+      self.id_prof = id_prof
+      self.senhap = senhap
 
-id_prof = ("Professor, Insira seu email ou nome do usuário: ")
-senhap = ("Agora, digite sua senha: ")
+print("====================TELA DE LOGIN PARA PROFESSOR====================\n")
+
+id_prof = input("Professor, Insira seu email ou nome do usuário: ")
+senhap = int(input("Agora, digite sua senha: "))
+
+print("====================================================================\n")
 
 class CadastroProf:
   def __init__ (self, nomeprof, data_nascimentoprof, sexoprof, emailprof, id_usuarioprof, senhaprof):
@@ -47,9 +63,13 @@ class CadastroProf:
     self.senhaprof = senhaprof
     self.emailprof = emailprof
 
-nomeprof = input("Informe o seu nome: ")
+print("====================TELA DE CADASTRO PARA PROFESSOR====================\n")
+
+nomeprof = input("Professor, Informe o seu nome: ")
 data_nascimentoprof = int(input("Digite sua Data de Nascimento: "))
 sexoprof = input("Informe o sexo: ")
-id.usuarioprof = input("Digite um nome para o usuário: ")
+id_usuarioprof = input("Digite um nome para o usuário: ")
 senhaprof = input("Crie uma senha: ")
 emailprof = input("Informe o seu email: ")
+
+print("=======================================================================\n")
